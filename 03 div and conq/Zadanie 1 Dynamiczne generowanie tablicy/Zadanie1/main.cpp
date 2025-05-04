f79090fa9b16866e8d12795c8a867e5001157eeb
31259 <31259@s.pm.szczecin.pl>
2025-05-04 12:52:20 +0200

    Update name

---
+++ 03 div and conq/Zadanie 1 Dynamiczne generowanie tablicy/Zadanie1/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream> 
+#include <cstdlib>
+using namespace std;
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void search(int arr[], int x) {
+    int l;
+    bool istnieje = false;
+    cout << "Podaj liczbe do wyszukiwania ";
+    cin >> l;
+
+    for (int y = 0; y < x; y++) {
+        if (arr[y] == l) {
+            cout << "Znaleziono pod indeksem: " << y << endl;
+            istnieje = true;
+        }
+    }
+    if (istnieje == false) {
+        cout << "Liczby " << l << " w tablice nie ma";
+    }
+}
+
+
+int main() {
+    int x, pocz, kon, roznica;
+    cout << "Podaj rozmiar tablicy ";
+    cin >> x;
+
+    cout << "Podaj poczotek zakresa: ";
+    cin >> pocz;
+
+    cout << "Podaj koniec zakresa: ";
+    cin >> kon;
+
+    roznica = kon - pocz;
+
+    int* table = new int[x];
+    for (int y = 0; y < x; y++) {
+        table[y] = rand() % roznica + pocz;
+    }
+
+    for (int y = 0; y < x; y++) {
+        cout << table[y] << " ";
+    }
+    cout << endl;
+
+    quickSort(table, 0, x - 1);
+    cout << "Posortowana tablica: " << endl;
+    printArray(table, x);
+
+    search(table, x);
+
+    return 0;
+}
